2f2e246931b5ac53307fe074a2f0e59ef2956ac5
SverreOK <sverre.o.k@hotmail.no>
2024-04-28 16:08:09 +0200

    sverreplot

---
+++ task2a_sverreplot.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Signal parameters
+amplitude = 1
+frequency = 100000
+angular_frequency = 2 * np.pi * frequency
+phase = np.pi / 8
+sample_frequency = 1000000
+period = 1 / sample_frequency
+
+# Samples
+N = 513
+
+# Time start index
+P = N * (N - 1) / 2
+Q = N * (N - 1) * (2 * N - 1) / 6
+t0 = P / Q
+
+# Time array
+t = np.linspace(t0, t0 + (N - 1) * period, N)
+
+# Complex exponential signal / Original signal
+s = amplitude * np.exp(1j * (angular_frequency * t + phase))
+
+# Initialize SNRdb and mean
+SNRdb = -20
+mean = 0
+
+# Create an array for the sample numbers
+samples = np.arange(N)
+
+plt.figure(figsize=(6,5))
+
+while SNRdb != 30:
+    # Increment SNRdb by 10
+    SNRdb += 10
+    SNR = 10 ** (SNRdb / 10)
+    std_dev = amplitude / np.sqrt(2 * SNR)
+    noise = np.random.normal(mean, std_dev, N) + 1j * np.random.normal(mean, std_dev, N)
+    x = s + noise
+    phase = np.angle(x)
+    phase = np.unwrap(phase)
+    plt.plot(samples, phase, label=f'SNRdb =  {SNRdb}')
+
+phase = np.angle(s)
+phase = np.unwrap(phase)
+plt.plot(samples, phase, label='Original signal')
+
+plt.xlabel('Samples')
+plt.ylabel('Phase (radians)')
+plt.title('Unwrapped Phase of Noisy Signal')
+plt.legend()
+plt.xlim(0, 500)
+plt.grid()
+plt.show()
